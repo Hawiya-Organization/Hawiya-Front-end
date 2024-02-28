07408d6051addb1b9120e15dcbf5e973545e1ff4
= <=>
2024-02-28 15:33:06 +0100

    setup

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+        colors:{
+                'green-darker': '#434B2F',
+                'green': '#858C36',
+                'green-light': '#BABE8E',
+                'red': '#EC0404',
+                'orange': '#FF7B02'
+        },
+        fontFamily:{
+                noto: ["Noto Sans Arabic", 'sans-serif'],
+                amiri : ["Amiri", 'serif'],
+        }
+    },
+  },
+  plugins: [],
+};
+export default config;
