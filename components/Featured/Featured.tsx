0970bbe4669bf9e610fe2dde384d7c50e16cd62d
= <=>
2024-04-13 00:57:15 +0200

    push 7asbiya allah

---
+++ components/Featured/Featured.tsx
@@ -0,0 +1,70 @@
+'use client'
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+import FeaturedCard from './Featured.card'
+import moufdi from '../../public/moufdi.png'
+
+export default function Featured() {
+        const pagination = {
+                clickable: true,
+                renderBullet: function (index: any, className: string) {
+                  return '<span class="custom-pagination-bullet ' + className + '">' + '' + '</span>';
+                },
+              };
+        const authors = [
+                {
+                        author: 'شاعر ١',
+                        biography: 'شاعر الثورة الجزائرية، ومؤلف النشيد الوطني الجزائري «قسما',
+                        pic: '../../public/moufdi.png'
+                },
+                {
+                        author: 'شاعر ٢',
+                        biography:  'شاعر الثورة الجزائرية، ومؤلف النشيد الوطني الجزائري «قسما',
+                        pic: '../../public/moufdi.png'
+                },
+                {
+                        author: 'شاعر٣',
+                        biography:  'شاعر الثورة الجزائرية، ومؤلف النشيد الوطني الجزائري «قسما',
+                        pic: '../../public/moufdi.png'
+                }
+        ]
+  return (
+    <div className="flex flex-col h-screen mt-10 gap-20 items-center justify-center relative">
+        <h2 className='text-4xl z-50'>مختارات</h2>
+        <div className='relative flex w-full items-center justify-center'>
+        <div className=" absolute rounded-full bg-opacity-80 bg-green-light blur-[50px] h-[500px] w-[500px] "></div>
+        <Swiper
+        initialSlide={1}
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={pagination}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        {authors.map((author, index)=>(
+                <SwiperSlide key={index}>
+                        <FeaturedCard author={author.author} pic={author.pic} biography={author.biography}></FeaturedCard>
+                </SwiperSlide>
+                ))}
+        </Swiper>
+        </div>
+
+       
+    </div>
+  )
+}
--- utils/Search.btn.tsx
+++ utils/Search.btn.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+'use client'
+import React, { FormEvent, useState } from 'react';
+import Image from 'next/image';
+import search from '../public/Hero/search.svg';
+import send from '../public/Hero/send.svg';
+import {motion} from 'framer-motion'
+import axios from 'axios';
 
-export default function SearchBtn() {
+interface IBtn{
+        onClick: (isHidden: boolean) => void;
+        onSubmit: (formData: FormData) => void;
+}
+
+export default function SearchBtn({onClick, onSubmit}: IBtn) {
+        const [input, setInput] = useState('');
+        const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+                event.preventDefault();
+                onSubmit(new FormData(event.currentTarget));
+              };
   return (
-    <div className="search-button w-[70px] h-[62px] ">
-
-    </div>
-  )
+        <form onSubmit={handleSubmit} className="search-button w-5/6 z-50 flex rounded-2xl justify-between bg-green-light p-2" style={{ boxShadow: '0 0 30px #848C36' }} action="">
+                        <button className="bg-green rounded-xl p-3">
+                                <Image width={32} src={send} alt="send" />
+                        </button>
+                        <motion.input
+                        onChange={e => setInput(e.target.name)}
+                         onAnimationStart={()=>onClick(false)} onAnimationEnd={()=>onClick(true)}  whileTap="tap"  className='bg-transparent font-amiri font-bold text-right outline-none px-5 flex w-full h-full' type="text" name='search' />
+                        <Image width={40} src={search} alt="search" />
+        </form>
+  );
 }
